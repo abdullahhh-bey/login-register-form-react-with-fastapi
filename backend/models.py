1b02636da99ac21ff903c6046ba915046e0ebb2d
abalex-ui <kibgheeo775533@gmail.com>
2025-10-12 02:39:31 +0500

    Adding table of Contract and relationship

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,7 @@
 from database import Base, engine
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import relationship, declarative_base, sessionmaker
+
 
 class User(Base):
     
@@ -10,6 +12,37 @@ class User(Base):
     email = Column(String(255),index=True , nullable=False, unique=True)
     hashed_pass = Column(String, nullable=False)
     is_verified = Column(Boolean , default=False)
+
+    sent_contacts = relationship(
+        "Contact",
+        foreign_keys="Contact.user_id",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
+    
+    received_contacts = relationship(
+        "Contact",
+        foreign_keys="Contact.friend_id",
+        back_populates="friend",
+        cascade="all, delete-orphan"
+    )
+    
+    def following(self):
+        
+        
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"),index=True)
+    friend_id = Column(Integer, ForeignKey("users.id"), index=True)
+    
+    __table_args__ = (UniqueConstraint("user_id", "friend_id", name="uq_user_friend"),)
+
+    user = relationship("User", foreign_keys=[user_id], back_populates="sent_contacts")
+    friend = relationship("User", foreign_keys=[friend_id], back_populates="received_contacts")
+    
 
 Base.metadata.create_all(bind=engine)
 
